ebbd6105d262d55bfe833cdd9cf55b8eabb66501
AustinLynes <austinlynes94@gmail.com>
2019-09-20 20:05:49 -0700

    bug fix

--- api/routes/authroutes.js
+++ api/routes/authroutes.js
@@ -6,39 +6,33 @@ const { validateUser } = require('../middleware/authmiddleware');
 
 const router = express.Router();
 
-router.post("/register-user", async (req, res) => {
+router.post("/register", async (req, res) => {
     const user = req.body;
 
-    //check to see if the payload is correct
-    if (!user.email || !user.password || user.email === "" || user.password === "") {
-        res.status(400).json({ message: 'Username and Password are both required to register a user' })
+    if (!user.email || !user.password || user.email === '' || user.password === ''){
+        res.status(400).json({ message: "Missing user information" });
     }
 
-    //hash the users password
     const hash = bcrypt.hashSync(user.password, 8);
     user.password = hash;
-
-    try {
-        //New User Authentication, if users email is fount then say sorry.. email already exists..
+    
+    try{
         const newUser = await userDb.loginUser(user.email);
-
-        if(newUser)
-            res.status(409).json({message: "Sorry this email already owns an account please try another Email"})
-
-        const regUser = {
-            name: user.name,
-            email: user.email,
-            password: user.password
-        }
-
+       
+        if (newUser)
+            res.status(409).json({ message: "User already exists" });
+        
+      
+        const regUser = {name: user.name, email: user.email, password: user.password };
+      
         const response = await userDb.registerUser(regUser);
-        const token = jwt.getToken({email: user.email, password: user.password, id: response})
-        if(response){
-            res.status(201).json({message: "User Data Created", token : token})
+        const token =  jwt.getToken({ email: user.email, password: user.password, id: response});
+        if (response){
+            res.status(200).json({ message: "User created", token: token });
         }
     }
-    catch (err) {
-        res.status(500).json({ message: err.message })
+    catch(error){
+        res.status(500).json({ error: error.message });
     }
 });
 
